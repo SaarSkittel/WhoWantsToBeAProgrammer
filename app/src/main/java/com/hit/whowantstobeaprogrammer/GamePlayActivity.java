46fc01921b76e231090963e166e5cadc8fcdcd7e
IdoAmira <68790040+IdoAmira@users.noreply.github.com>
2021-07-06 12:22:57 +0300

    06/07/21 12:22

--- app/src/main/java/com/hit/whowantstobeaprogrammer/GamePlayActivity.java
+++ app/src/main/java/com/hit/whowantstobeaprogrammer/GamePlayActivity.java
@@ -36,7 +36,12 @@ public class GamePlayActivity extends AppCompatActivity {
     AnswerButton Answer;
     TextView QuestionTV;
     ArrayList<String>Answers;
-    ArrayList<Integer>ids;
+    ArrayList<Integer>easyIds;
+    ArrayList<Integer>mediumIds;
+    ArrayList<Integer>hardIds;
+    int level = 1;
+
+
     CountDownTimer cTimer = null;
     TextView Name;
     private int score;
@@ -88,26 +93,16 @@ public class GamePlayActivity extends AppCompatActivity {
         backgroundMusic = new MediaPlayer();
         backgroundMusic = MediaPlayer.create(GamePlayActivity.this,R.raw.background);
         score=sp.getInt("score",0);
-        Name = findViewById(R.id.name_score);
+        Name = findViewById(R.id.score);
         Name.setText(sp.getString("user_name","")+", Score:"+ score);
+        SplitLevelQuestions();
 
-        Field [] fields= R.array.class.getFields();
-        ids= new ArrayList<Integer>(fields.length);
-
-        for (int i=0;i<fields.length;++i){
-            ids.add(Integer.valueOf(getResources().getIdentifier("array/" + fields[i].getName(), null, getPackageName())));
-        }
-
-        if(musicOnOrOff){
-            backgroundMusic.start();
-            musicControl();
-        }
         QuestionTV=findViewById(R.id.question_tv);
         Answer1=findViewById(R.id.answer1_btn);
         Answer2=findViewById(R.id.answer2_btn);
         Answer3=findViewById(R.id.answer3_btn);
         Answer4=findViewById(R.id.answer4_btn);
-        music =findViewById(R.id.test);
+        music =findViewById(R.id.music_game_btn);
         music.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -152,7 +147,9 @@ public class GamePlayActivity extends AppCompatActivity {
     private void answerCheck(AnswerButton clicked){
         clicked.ButtonSelected();
         if(clicked == Answer){
-            score += 100;
+            score+= level==1?100:level==2?200:300;
+            level++;
+            //score += 100;
             clicked.CorrectAnswer(true);
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
@@ -187,54 +184,70 @@ public class GamePlayActivity extends AppCompatActivity {
         return intList.toArray(intArray);
     }
 
-    private int randomizeQuestion(){
+    private int randomizeQuestion(ArrayList<Integer> curIds){
         Random r = new Random();
-        int result = r.nextInt(ids.size() + 0);
+        int result = r.nextInt(curIds.size() + 0);
         return result;
     }
     private void gameOver(){
         SharedPreferences.Editor editor = sp.edit();
         editor.putInt("score",score);
+        editor.putBoolean("status",musicOnOrOff);
+        editor.commit();
         Intent intent = new Intent(GamePlayActivity.this,GameOverActivity.class);
         startActivity(intent);
         finish();
 
     }
     private void load(){
-        cTimer.cancel();
-        cTimer.start();
-        int questionNumber = randomizeQuestion();
-        String [] str=getResources().getStringArray(ids.get(questionNumber).intValue());
-        ids.remove(questionNumber);
-        Answers = new ArrayList<>();
-        Answers.add(str[1]);
-        Answers.add(str[2]);
-        Answers.add(str[3]);
-        Answers.add(str[4]);
-        QuestionTV.setText(str[0]);
-        Integer[] ran = randomizeAnswers();
-        Answer1.setText(Answers.get(ran[0]));
-        Answer2.setText(Answers.get(ran[1]));
-        Answer3.setText(Answers.get(ran[2]));
-        Answer4.setText(Answers.get(ran[3]));
-        int indexAnswer = -1;
-        for(int i=0;i<4;++i){
-            if(ran[i]==0){
-                indexAnswer =i;
-                break;
+        if(!hardIds.isEmpty()) {
+            cTimer.cancel();
+            cTimer.start();
+            Name.setText(score+"");
+            String [] str = null;
+            if (level == 1) {
+                int questionNumber = randomizeQuestion(easyIds);
+                str = getResources().getStringArray(easyIds.get(questionNumber).intValue());
+                easyIds.remove(questionNumber);
+            } else if (level == 2) {
+                int questionNumber = randomizeQuestion(mediumIds);
+                str = getResources().getStringArray(mediumIds.get(questionNumber).intValue());
+                mediumIds.remove(questionNumber);
+            } else if (level == 3) {
+                int questionNumber = randomizeQuestion(hardIds);
+                str = getResources().getStringArray(hardIds.get(questionNumber).intValue());
+                hardIds.remove(questionNumber);
+            }
+            Answers = new ArrayList<>();
+            Answers.add(str[1]);
+            Answers.add(str[2]);
+            Answers.add(str[3]);
+            Answers.add(str[4]);
+            QuestionTV.setText(str[0]);
+            Integer[] ran = randomizeAnswers();
+            Answer1.setText(Answers.get(ran[0]));
+            Answer2.setText(Answers.get(ran[1]));
+            Answer3.setText(Answers.get(ran[2]));
+            Answer4.setText(Answers.get(ran[3]));
+            int indexAnswer = -1;
+            for (int i = 0; i < 4; ++i) {
+                if (ran[i] == 0) {
+                    indexAnswer = i;
+                    break;
+                }
+            }
+            if (indexAnswer == 0) {
+                Answer = Answer1;
+            } else if (indexAnswer == 1) {
+                Answer = Answer2;
+            } else if (indexAnswer == 2) {
+                Answer = Answer3;
+            } else if (indexAnswer == 3) {
+                Answer = Answer4;
             }
         }
-        if(indexAnswer==0){
-            Answer=Answer1;
-        }
-        else if(indexAnswer==1){
-            Answer=Answer2;
-        }
-        else if(indexAnswer==2){
-            Answer=Answer3;
-        }
-        else if(indexAnswer==3){
-            Answer=Answer4;
+        else{
+            gameOver();
         }
     }
     private void musicControl() {
@@ -246,6 +259,27 @@ public class GamePlayActivity extends AppCompatActivity {
             backgroundMusic.start();
             music.setImageResource(android.R.drawable.ic_lock_silent_mode_off);
         }
+    }
+
+    private void SplitLevelQuestions(){
+        Field [] fields= R.array.class.getFields();
+        easyIds= new ArrayList<Integer>();
+        mediumIds= new ArrayList<Integer>();
+        hardIds= new ArrayList<Integer>();
+        for (int i=0;i<fields.length;++i){
+            String questionName = fields[i].getName();
+            char current = questionName.charAt(0);
+            if(current == 'E'){
+                easyIds.add(Integer.valueOf(getResources().getIdentifier("array/" + fields[i].getName(), null, getPackageName())));
+            }
+            else if(current == 'M'){
+                mediumIds.add(Integer.valueOf(getResources().getIdentifier("array/" + fields[i].getName(), null, getPackageName())));
+            }
+            else if(current == 'H'){
+                hardIds.add(Integer.valueOf(getResources().getIdentifier("array/" + fields[i].getName(), null, getPackageName())));
+            }
+        }
+
     }
 
 
